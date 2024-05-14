b7df60a31e3540bd07116369f10a4dd576733ea6
Elton Saravia <ryuzaki12elton@gmail.com>
2024-05-14 23:53:38 +0200

    Calendario aplicada expacioón para descripcion al precionar encima

--- app/src/main/java/com/example/gabi/AdaptadorEvento.java
+++ app/src/main/java/com/example/gabi/AdaptadorEvento.java
@@ -4,15 +4,19 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
 import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.List;
+
 import dto.EventoDTO;
 
 public class AdaptadorEvento extends RecyclerView.Adapter<AdaptadorEvento.EventoViewHolder> {
-
     private Context context;
     private List<EventoDTO> eventos;
+    private int expandedPosition = -1;
 
     public AdaptadorEvento(Context context, List<EventoDTO> eventos) {
         this.context = context;
@@ -26,22 +30,34 @@ public class AdaptadorEvento extends RecyclerView.Adapter<AdaptadorEvento.Evento
     }
 
     @Override
-    public void onBindViewHolder(EventoViewHolder holder, int position) {
+    public void onBindViewHolder(final EventoViewHolder holder, int position) {
         EventoDTO evento = eventos.get(position);
-        holder.tvHoraEvento.setText(evento.getHoraCita().toString()); // Convertimos Time a String
+        holder.tvHoraEvento.setText(evento.getHoraCita().toString());
         holder.tvMotivoEvento.setText(evento.getMotivoCita());
         holder.tvLugarEvento.setText(evento.getLugarCita());
         holder.tvDetallesEvento.setText(evento.getDetalles());
-    }
 
+        final boolean isExpanded = holder.getAdapterPosition() == expandedPosition;
+        holder.expandableLayout.setVisibility(isExpanded ? View.VISIBLE : View.GONE);
+        holder.itemView.setActivated(isExpanded);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                expandedPosition = isExpanded ? -1 : holder.getAdapterPosition();
+                notifyDataSetChanged();
+            }
+        });
+    }
 
     @Override
     public int getItemCount() {
         return eventos.size();
     }
 
-    public class EventoViewHolder extends RecyclerView.ViewHolder {
+    class EventoViewHolder extends RecyclerView.ViewHolder {
         TextView tvHoraEvento, tvMotivoEvento, tvLugarEvento, tvDetallesEvento;
+        LinearLayout expandableLayout;
 
         public EventoViewHolder(View itemView) {
             super(itemView);
@@ -49,6 +65,7 @@ public class AdaptadorEvento extends RecyclerView.Adapter<AdaptadorEvento.Evento
             tvMotivoEvento = itemView.findViewById(R.id.tvMotivoEvento);
             tvLugarEvento = itemView.findViewById(R.id.tvLugarEvento);
             tvDetallesEvento = itemView.findViewById(R.id.tvDetallesEvento);
+            expandableLayout = itemView.findViewById(R.id.expandableLayout);
         }
     }
 }
